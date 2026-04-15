dff56e5df956315cbc0ad582b53ea32bf307d9ac
2025050190 <bria051@kau.kr>
2026-04-15 15:20:06 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n = 0, count = 0, answer = 0;
+    scanf("%d", &answer);
+    do {
+        scanf("%d", &n);
+        if (n > answer)
+            printf("%d>?\n");
+        else if (n < answer)
+            printf("%d<?\n");
+        else
+            printf("%d==?\n", n);
+        count ++;
+    } while (n != answer);
+    printf("%d", count);
+
+    return 0;
+}
