467fa74cc0f4ff4933c10877f79bd8872834e540
2025050190 <bria051@kau.kr>
2026-04-15 15:34:01 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int N = 0, count_char = 0, count_int = 0, max_char = 0, max_int = 0;
+    char ch = '\0';
+
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i ++) {
+        scanf(" %c", &ch);
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            count_char ++;
+            if (max_int < count_int) {
+                max_int = count_int;
+            }
+            count_int = 0;
+
+        }
+        else if (ch >= '0' && ch <= '9') {
+            count_int ++;
+            if (max_char < count_char) {
+                max_char = count_char;
+            }
+            count_char = 0;
+
+        }
+    }
+
+    if (max_char < count_char)
+        max_char = count_char;
+    if (max_int < count_int)
+        max_int = count_int;
+
+    printf("%d\n%d", max_char, max_int);
+
+    return 0;
+}
